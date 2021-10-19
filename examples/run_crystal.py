7e6a6187ff21155520298092475f7cc3c5ba0053
Manuel Sanchez Del Rio <srio@esrf.eu>
2021-10-19 17:07:36 +0200

    xopy files

---
+++ examples/run_crystal.py
@@ -0,0 +1,50 @@
+from xoppylib.xoppy_calc_xcrystal import xoppy_calc_xcrystal
+
+if __name__ == "__main__":
+    import numpy
+    from PyQt5.QtWidgets import QApplication
+    from srxraylib.plot.gol import plot
+    from orangecontrib.xoppy.widgets.optics.xcrystal import OWxcrystal
+
+    tmp = xoppy_calc_xcrystal(
+        CRYSTAL_MATERIAL=32,
+        MILLER_INDEX_H=1,
+        MILLER_INDEX_K=1,
+        MILLER_INDEX_L=1,
+        TEMPER=1,
+        MOSAIC=0,
+        GEOMETRY=0,
+        SCAN=2,
+        UNIT=1,
+        SCANFROM=-100,
+        SCANTO=150,
+        SCANPOINTS=200,
+        ENERGY=5000,
+        ASYMMETRY_ANGLE=0.0,
+        THICKNESS=1.0,
+        MOSAIC_FWHM=0,
+        RSAG=0,
+        RMER=0,
+        ANISOTROPY=0,
+        POISSON=0,
+        CUT="",
+        FILECOMPLIANCE="",
+    )
+
+    a = numpy.loadtxt("diff_pat.dat", skiprows=5)
+    # L  Th-ThB{in} [microrad]  Th-ThB{out} [microrad]  phase_p[rad]  phase_s[rad]  Circ Polariz  p-polarized  s-polarized
+    print(a.shape)
+    # plot(a[:,0],a[:,-1],title="Energy = %f eV "%energy)
+
+    step = a[1, 0] - a[0, 0]
+    h = a[:, -1]
+    peak = a[:, -1].max()
+    integrated_intensity = a[:, -1].sum() * step
+    tt = numpy.where(h >= max(h) * 0.5)
+    fwhm = step * (tt[0][-1] - tt[0][0])
+
+    print("Peak value = ", peak)
+    print("Integrated intensity [urad] = ", integrated_intensity)
+    print("FWHM=", fwhm)
+
+    plot(a[:, 0], a[:, -1], xtitle="anggle [urad]", ytitle="reflectivity")
---
+++ xoppylib/xoppy_util.py
@@ -0,0 +1,127 @@
+import sys, os, numpy, platform, six
+# import orangecanvas.resources as resources
+# from PyQt5 import QtGui, QtCore
+
+try:
+    import matplotlib
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    from matplotlib import figure as matfig
+    import pylab
+except ImportError:
+    print(sys.exc_info()[1])
+    pass
+
+
+import xraylib
+
+from oasys.widgets import gui
+from orangecontrib.xoppy.util.text_window import TextWindow
+
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QPlainTextEdit
+#
+# class EmittingStream(QtCore.QObject):
+#     textWritten = QtCore.pyqtSignal(str)
+#
+#     def write(self, text):
+#         self.textWritten.emit(str(text))
+
+def package_dirname(package):
+    """Return the directory path where package is located.
+
+    """
+    if isinstance(package, six.string_types):
+        package = __import__(package, fromlist=[""])
+    filename = package.__file__
+    dirname = os.path.dirname(filename)
+    return dirname
+
+class locations:
+    @classmethod
+    def home_bin(cls):
+        if platform.system() == "Windows":
+            return package_dirname("orangecontrib.xoppy.util") + "\\bin\windows\\"
+        else:
+            return package_dirname("orangecontrib.xoppy.util") + "/bin/" + str(sys.platform) + "/"
+
+    @classmethod
+    def home_doc(cls):
+        if platform.system() == "Windows":
+            return package_dirname("orangecontrib.xoppy.util") + "\doc_txt/"
+        else:
+            return package_dirname("orangecontrib.xoppy.util") + "/doc_txt/"
+
+    @classmethod
+    def home_data(cls):
+        if platform.system() == "Windows":
+            return package_dirname("orangecontrib.xoppy.util") + "\data/"
+        else:
+            return package_dirname("orangecontrib.xoppy.util") + "/data/"
+
+    @classmethod
+    def home_bin_run(cls):
+        #return resources.package_dirname("orangecontrib.xoppy.util") + "/bin_run/"
+        return os.getcwd()
+
+def xoppy_doc(app):
+    home_doc = locations.home_doc()
+
+    filename1 = os.path.join(home_doc,app+'.txt')
+
+    o = TextWindow()
+    o.set_file(filename1)
+
+
+class XoppyPhysics:
+
+    ######################################
+    # FROM NIST
+    codata_h = numpy.array(6.62606957e-34)
+    codata_ec = numpy.array(1.602176565e-19)
+    codata_c = numpy.array(299792458.0)
+    ######################################
+
+    A2EV = (codata_h*codata_c/codata_ec)*1e+10
+    K2EV = 2*numpy.pi/(codata_h*codata_c/codata_ec*1e+2)
+
+    @classmethod
+    def getWavelengthFromEnergy(cls, energy): #in eV
+        return cls.A2EV/energy # in Angstrom
+
+    @classmethod
+    def getEnergyFromWavelength(cls, wavelength): # in Angstrom
+        return cls.A2EV/wavelength # in eV
+
+    @classmethod
+    def getMaterialDensity(cls, material_formula):
+        if material_formula is None: return 0.0
+        if str(material_formula.strip()) == "": return 0.0
+
+        try:
+            compoundData = xraylib.CompoundParser(material_formula)
+
+            if compoundData["nElements"] == 1:
+                return xraylib.ElementDensity(compoundData["Elements"][0])
+            else:
+                return 0.0
+        except:
+            return 0.0
+
+class XoppyGui:
+
+    @classmethod
+    def combobox_text(cls, widget, master, value, box=None, label=None, labelWidth=None,
+             orientation='vertical', items=(), callback=None,
+             sendSelectedValue=False, valueType=str,
+             control2attributeDict=None, emptyString=None, editable=False, selectedValue = None,
+             **misc):
+
+        combo = gui.comboBox(widget, master, value, box=box, label=label, labelWidth=labelWidth, orientation=orientation,
+                                  items=items, callback=callback, sendSelectedValue=sendSelectedValue, valueType=valueType,
+                                  control2attributeDict=control2attributeDict, emptyString=emptyString, editable=editable, **misc)
+        try:
+            combo.setCurrentIndex(items.index(selectedValue))
+        except:
+            pass
+
+        return combo
